c670535b85389d24c5d2c664a6887cf57867a28f
aaristov <aaristov@pasteur.fr>
2019-04-03 17:55:44 +0200

    fix imports

--- fastspt/__init__.py
+++ fastspt/__init__.py
@@ -1,9 +1,9 @@
 from fastspt import *
 from fastspt.version import __version__
 
-from fastspt import fastSPT_tools, readers, writers
+from fastspt import tools, readers, writers
 try:
-	import fastSPT_plot
+	from fastspt import plot
 except Exception as e:
 	print("Could not import the plot submodule, error:")
 	print(e)
