160acb18dbbd4344ff1fbd949c30442c06bf0c4f
Andrey Aristov <andrey@lumix.mcm.pasteur.fr>
2019-05-14 15:56:04 +0200

    update requirements

--- setup.py
+++ setup.py
@@ -9,15 +9,13 @@ from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
-#import fastspt.version as version
+from fastspt.version import __version__
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-#execfile("fastspt/version.py")
-exec(open("fastspt/version.py").read())
 print("Version", __version__)
 setup(
     name='fastspt',
@@ -34,7 +32,7 @@ setup(
     url='https://gitlab.com/tjian-darzacq-lab/Spot-On-cli',
 
     # Author details
-    author='Maxime Woringer, Anders Sejr Hansen',
+    author='Maxime Woringer, Anders Sejr Hansen, Andrey Aristov',
     author_email='maxime@woringer.fr',
 
     # Choose your license
@@ -56,8 +54,8 @@ setup(
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
@@ -76,7 +74,15 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['numpy', 'scipy', 'lmfit'],
+    install_requires=['numpy', 
+                        'scipy', 
+                        'lmfit', 
+                        'matplotlib', 
+                        'xmltodict',
+                        'pandas',
+                        'xlwt',
+                        'tqdm',
+                        'mat4py'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
