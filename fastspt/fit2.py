733a2c49617ec06f2324fd9e8f2f1098d4c2b2a5
aaristov <aaristov@pasteur.fr>
2019-11-01 00:53:09 +0100

    faster spoton fit with explicit parameters

---
+++ fastspt/fit2.py
@@ -0,0 +1,238 @@
+import lmfit
+import numpy as np
+import matplotlib.pyplot as plt
+from fastspt import bayes
+from tqdm.auto import tqdm
+
+
+class JumpLengthHistogram:
+    '''
+    Object containing histogram values, bin edges, centers of bins 
+    and time lag
+    '''
+    def __init__(self, hist, bin_edges, lag:int):
+        self.hist = hist
+        assert int(lag) > 0
+        self.lag = int(lag)
+        self.bin_edges = bin_edges
+        self.vector = bin_edges[:-1] + np.diff(bin_edges[:2])[0] / 2
+        assert len(self.vector) == len(self.hist)
+    
+    def __repr__(self):
+        return f"""JumpLengthHistogram: \n\tlag: {self.lag} \n\tvector: {self.vector} \n\tvalues: {self.hist}"""
+    
+    def __len__(self):
+        return len(self.vector)
+
+
+def fit_spoton_2_0(
+    tracks=None, 
+    path=None, 
+    n_lags=1, 
+    plot=True, 
+    dt=0.06,
+    D=(0, 0.1), 
+    fit_D=(False, True), 
+    F=(0.5, 1 - 0.5), 
+    fit_F=(True, True),
+    sigma=0.1, 
+    fit_sigma=True,
+    n_bins=50,
+    max_um=0.6,
+    verbose=False,
+    **kwargs
+) -> dict:
+
+    if verbose:
+        print(f'Fit {path}')
+    n_tracks = len(tracks)
+    hists = get_jds_histograms(tracks, n_lags, bins=n_bins, max_um=max_um, disable_tqdm=not verbose) 
+
+    fit_result = fit_jd_hist(
+        hists=hists, 
+        dt=dt, 
+        D=D, 
+        fit_D=fit_D, 
+        F=F, 
+        fit_F=fit_F,
+        sigma=sigma, 
+        fit_sigma=fit_sigma,
+        verbose=verbose
+    )
+
+    values = fit_result.params.valuesdict()
+    dt = values['dt']
+    sigma = values['sigma']
+    D_free = values['D2']
+    D_bound = values['D1']
+    D_all = [values[f'D{i+1}'] for i in range(len(D))]
+    F_all = [values[f'F{i+1}'] for i in range(len(F))]
+    order = np.argsort(D_all)
+    D_all = np.array(D_all)[order]
+    F_all = np.array(F_all)[order]
+    F_bound = values['F1']
+    
+
+    if plot:
+        print(D_all, F_all)
+        _ = [get_error_histogram_vs_model(h, dt, sigma, D_all, F_all, plot=True, use_cdf=False) for h in hists]
+        # _ = [get_error_histogram_vs_model(h, dt, sigma, (0,D_free), (F_bound, 1-F_bound), plot=True, use_cdf=True) for h in hists]
+    return {
+        'sigma': sigma, 
+        'D_free': D_free, 
+        'D_bound': D_bound,
+        'F_bound': F_bound, 
+        'D_all': D_all,
+        'F_all': F_all,
+        'n_tracks': n_tracks, 
+        'chi2': fit_result.chisqr, 
+        'chi2_norm': fit_result.chisqr / n_bins / n_lags,
+        'n_iter': fit_result.nfev,
+        'path': path,
+        **kwargs}
+
+
+def fit_jd_hist(
+    hists:list, 
+    dt:float, 
+    D:list, 
+    fit_D:list, 
+    F:list,
+    fit_F:list,
+    sigma:float, 
+    fit_sigma:bool, 
+    verbose=False, 
+    
+):
+    
+    '''
+    Fits jd probability functions to a jd histograms. 
+    
+    Parameters:
+    hist (list): histogram values
+    D (list): init values for MSD
+    F (list): fractions for D, sum = 1
+    sigma (float): localization precision guess
+    funcs (dict): dictionary with functions sigma, gamma, center, amplitude
+    
+    Returns:
+    popt (lmfit.minimizerResult): optimized parameters
+    '''
+    from lmfit import Parameters, Parameter, fit_report, minimize
+            
+    def residual(fit_params, data):
+        res = cumulative_error_jd_hist(fit_params, data, len(D))
+        return res
+    
+    fit_params = Parameters()
+    
+    fit_params.add('sigma', value=sigma, vary=fit_sigma, min=0.)
+    fit_params.add('dt', value=dt, vary=False)
+    fit_params.add('max_lag', value=max([h.lag for h in hists]), vary=False)
+    
+    for i, (d, f_d, f, f_f) in enumerate(zip(D, fit_D, F, fit_F)):
+        fit_params.add(f'D{i+1}', value=d, vary=f_d, min=0.)
+        fit_params.add(f'F{i+1}', value=f, min=0., max=1., vary=f_f)
+        
+    f_expr = '1'
+    for i, f in enumerate(F[:-1]):
+        f_expr += f' - F{i+1}'
+
+    fit_params[f'F{len(F)}'] = Parameter(name=f'F{i+1}', min=0., max=1., expr=f_expr) 
+    
+    
+    if verbose:
+        print('start minimize')
+    try:
+        # print(fit_params)
+
+        minimizer_result  = minimize(residual, fit_params, args=(hists, ))#, **solverparams)
+
+        if verbose:
+            print(f'completed in {minimizer_result.nfev} steps')
+            minimizer_result.params.pretty_print()
+
+    except ValueError as e:
+        fit_params.pretty_print()
+        raise e
+
+    return minimizer_result
+       
+        
+def get_jds_histograms(tracks, max_lag, max_um=0.6, bins=100, disable_tqdm=False):
+    '''
+    For every lag in 1..max_lag compute density histogram
+    
+    '''
+    def single_hist(i):
+        lag = i + 1
+        _jds = [bayes.get_jd(t.xy, lag, filter_frame_intevals=t.frame) for t in tracks]
+        jds = np.concatenate(_jds, axis=0)
+        h, edges = np.histogram(jds, bins=bins, range=(0, max_um), density=True)
+        return JumpLengthHistogram(h, edges, lag)
+        
+    hists = list(map(single_hist, tqdm(range(max_lag), desc=f'jds hists for {max_lag} lags', disable=disable_tqdm)))
+    
+    return hists
+
+def get_error_histogram_vs_model(
+    hist:JumpLengthHistogram, 
+    dt:float, 
+    sigma:float, 
+    D:list, 
+    F:list, 
+    use_cdf=False,
+    p_density=bayes.p_jd, 
+    plot=False
+):
+    assert len(D) == len(F)
+    vector = hist.vector
+    values = hist.hist
+    lag = hist.lag
+    model = np.zeros_like(vector)
+    
+    for _D, _F in zip(D, F):
+#         print('_D,_F: ',_D, _F)
+        model = model + p_density(dt * lag, sigma, _D)(vector) * _F
+    
+    if use_cdf:
+        model = np.cumsum(model)
+        values = np.cumsum(values)
+    
+    if plot:
+        plt.figure(figsize=(10, 1))
+        print(D, F)
+        for i, (_D, _F) in enumerate(zip(D, F)):
+            plt.plot(
+                vector, 
+                p_density(dt * lag, sigma, _D)(vector) * _F, 
+                alpha=0.5, 
+                label=f'D{i}: {_D:.3f} ({_F:.0%})'
+            )
+        plt.plot(vector, model, 'r-', label='sum model')
+        plt.bar(vector, values, width=np.diff(vector)[0], label=f'jd {lag} lag', fill=None)
+        plt.title(f'sigma: {sigma:0.3f}')
+        plt.legend()
+        plt.show()
+        
+    return model - values
+
+def cumulative_error_jd_hist(fit_params:lmfit.Parameters, hist_list, num_states):
+    
+    p = fit_params.valuesdict()
+#     print(p)
+    cum = [
+        get_error_histogram_vs_model(
+            h, 
+            dt=p["dt"], 
+            sigma=p["sigma"], 
+            D=list(p[f'D{i+1}'] for i in range(num_states)), 
+            F=list(p[f'F{i+1}'] for i in range(num_states)),
+            plot=False
+        ) 
+        for h in hist_list
+    ]
+#     [plt.plot(c) for c in cum]
+    return np.concatenate(cum, axis=0)
+#     p = fit_params.valuesdict()
+    
---
+++ tests/test_fit2.py
@@ -0,0 +1,61 @@
+import pytest
+from fastspt import fit2, simulate
+import numpy as np
+
+sim_params = dict(
+    num_tracks=1e3, 
+    dt=0.06, 
+    D_free=0.06, 
+    loc_error=0.02, 
+    p_binding=3e-2, 
+    p_unbinding=1e-1, 
+    p_bleaching=1e-1, 
+    p_out_of_focus=1e-5, 
+    min_len=5, 
+    use_tqdm=False
+    )
+
+sim_params['F_bound'] = sim_params['p_binding']/(sim_params['p_binding'] + sim_params['p_unbinding'])
+
+fit_params = dict(
+    n_lags=1, 
+    plot=False, 
+    dt=0.06, 
+    D=(0, 0.1), 
+    fit_D=(False, True), 
+    F=(0.5, 1 - 0.5), 
+    fit_F=(True, True), 
+    sigma=0.1, 
+    fit_sigma=True, 
+    n_bins=50, 
+    max_um=0.6, 
+    verbose=False
+)
+
+
+class TestFit:
+
+    
+    def test_sim_tracks(self):
+        tracks = simulate.tracks(**sim_params)
+        assert len(tracks) == sim_params['num_tracks']
+
+    def test_fit_tracks(self):
+        sim_params['num_tracks'] = 1e4
+        tracks = simulate.tracks(**sim_params)
+        fit = fit2.fit_spoton_2_0(tracks, **fit_params)
+
+        np.testing.assert_almost_equal(
+            fit['D_free'], sim_params['D_free'], 
+            decimal=2,
+            err_msg=f"D_free input: {sim_params['D_free']}, fit: {fit['D_free']}")
+
+        np.testing.assert_almost_equal(fit['sigma'], sim_params['loc_error'], decimal=2)
+
+        np.testing.assert_almost_equal(
+            fit['F_bound'], sim_params['F_bound'],         
+            decimal=2, 
+            err_msg=f"Fitted F_bound {fit['F_bound']:.2f}, simulated {sim_params['F_bound']:.2f}"
+        )
+
+        
