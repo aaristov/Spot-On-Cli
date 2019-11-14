831646152519079f53ca09f7d16be323405d53d8
aaristov <aaristov@pasteur.fr>
2019-11-14 17:23:09 +0100

    add dual-sigma fit tests

--- fastspt/tests/test_fit2.py
+++ fastspt/tests/test_fit2.py
@@ -25,8 +25,8 @@ fit_params = dict(
     fit_D=(False, True), 
     F=(0.5, 1 - 0.5), 
     fit_F=(True, True), 
-    sigma=0.1, 
-    fit_sigma=True, 
+    sigma=(0.1,), 
+    fit_sigma=(True,), 
     n_bins=50, 
     max_um=0.6, 
     verbose=False
@@ -36,32 +36,81 @@ tracks = simulate.tracks(**sim_params)
 
 fit = fit2.fit_spoton_2_0(tracks, **fit_params)
 
-class TestFit:
 
+def test_sim_tracks():
+    assert len(tracks) == sim_params['num_tracks']
+
+def test_fit_tracks():
+
+    np.testing.assert_almost_equal(
+        fit['D'][1], sim_params['D_free'], 
+        decimal=2,
+        err_msg=f"D_free input: {sim_params['D_free']}, fit: {fit['D'][1]}")
+
+    np.testing.assert_almost_equal(fit['sigma'][0], sim_params['loc_error'], decimal=2)
+
+    np.testing.assert_almost_equal(
+        fit['F'][0], sim_params['F_bound'],         
+        decimal=1, 
+        err_msg=f"Fitted F_bound {fit['F'][0]:.2f}, simulated {sim_params['F_bound']:.2f}"
+    )
+
+def test_fit_jd_hist_bad_input():
+    with pytest.raises(TypeError):
+        fit2.fit_jd_hist(None, 0, [0,0], [1,1], [0,1], [1,1], 0, 1, 0)
+
+    with pytest.raises(AttributeError):
+        fit2.fit_jd_hist([None], 0, [0,0], [1,1], [0,1], [1,1], 0, 1, 0)
+
+
+def test_fit_2_sigmas(sigmas = [0.01, 0.03], n_tracks=5000, D_free=0.1, plot=False):
+    '''
+    Simulate confined state
+    '''
+    sim1 = simulate.tracks(
+        n_tracks=n_tracks, 
+        D_free=D_free,
+        loc_error=sigmas[0],
+        use_tqdm=False)
+
+    sim2 = simulate.tracks(
+        n_tracks=n_tracks, 
+        D_free=D_free,
+        loc_error=sigmas[1],
+        use_tqdm=False)
+
+    sim = sim1 + sim2
+
+    fit_2_sigmas = fit2.fit_spoton_2_0(
+        sim,
+        D=[0, 0.0, 0.1],
+        F=[0.3, 0.3, 0.4],
+        fit_D=[False, False, True],
+        fit_F=[1,1,1],
+        sigma=[0.01, 0.04],
+        fit_sigma=[True, True],
+        n_bins=100,
+        dt=0.06,
+        n_lags=3,
+        plot=plot)
+
+    fit_1_sigmas = fit2.fit_spoton_2_0(
+        sim,
+        D=[0, 0.1],
+        F=[0.3, 0.7],
+        fit_D=[False, True],
+        fit_F=[1,1],
+        sigma=[0.01],
+        fit_sigma=[True, True],
+        n_bins=100,
+        dt=0.06,
+        n_lags=3,
+        plot=plot)
+
+    for s, S in zip(sigmas, fit_2_sigmas['sigma']):
+        np.testing.assert_almost_equal(S, s, 2)
     
-    def test_sim_tracks(self):
-        assert len(tracks) == sim_params['num_tracks']
+    assert fit_1_sigmas['chi2_norm'] > fit_2_sigmas['chi2_norm']
 
-    def test_fit_tracks(self):
+    return fit_2_sigmas
 
-        np.testing.assert_almost_equal(
-            fit['D_free'], sim_params['D_free'], 
-            decimal=2,
-            err_msg=f"D_free input: {sim_params['D_free']}, fit: {fit['D_free']}")
-
-        np.testing.assert_almost_equal(fit['sigma'], sim_params['loc_error'], decimal=2)
-
-        np.testing.assert_almost_equal(
-            fit['F_bound'], sim_params['F_bound'],         
-            decimal=1, 
-            err_msg=f"Fitted F_bound {fit['F_bound']:.2f}, simulated {sim_params['F_bound']:.2f}"
-        )
-
-    def test_fit_jd_hist_bad_input(self):
-        with pytest.raises(TypeError):
-            fit2.fit_jd_hist(None, 0, [0,0], [1,1], [0,1], [1,1], 0, 1, 0)
-
-        with pytest.raises(AttributeError):
-            fit2.fit_jd_hist([None], 0, [0,0], [1,1], [0,1], [1,1], 0, 1, 0)
-
-        
